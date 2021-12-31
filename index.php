545ea0c85890b19f265b654e098425ec8e5099b5
Habibul Islam <habibulislam6862@gmail.com>
2021-12-31 11:49:31 +0600

    updated

--- index.php
+++ index.php
@@ -1,83 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Basic Website</title>
-	<meta name="language" content="English">
-	<meta name="description" content="It is a website about education">
-	<meta name="keywords" content="blog,cms blog">
-	<meta name="author" content="Delowar">
-	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
-	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="style.css">
-	<script src="js/jquery.js" type="text/javascript"></script>
-	<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
-
-<script type="text/javascript">
-$(window).load(function() {
-	$('#slider').nivoSlider({
-		effect:'random',
-		slices:10,
-		animSpeed:500,
-		pauseTime:5000,
-		startSlide:0, //Set starting Slide (0 index)
-		directionNav:false,
-		directionNavHide:false, //Only show on hover
-		controlNav:false, //1,2,3...
-		controlNavThumbs:false, //Use thumbnails for Control Nav
-		pauseOnHover:true, //Stop animation while hovering
-		manualAdvance:false, //Force manual transitions
-		captionOpacity:0.8, //Universal caption opacity
-		beforeChange: function(){},
-		afterChange: function(){},
-		slideshowEnd: function(){} //Triggers after all slides have been shown
-	});
-});
-</script>
-</head>
-
-<body>
-	<div class="headersection templete clear">
-		<a href="#">
-			<div class="logo">
-				<img src="images/logo.png" alt="Logo"/>
-				<h2>Website Title</h2>
-				<p>Our website description</p>
-			</div>
-		</a>
-		<div class="social clear">
-			<div class="icon clear">
-				<a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-				<a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-				<a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
-				<a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
-			</div>
-			<div class="searchbtn clear">
-			<form action="" method="post">
-				<input type="text" name="keyword" placeholder="Search keyword..."/>
-				<input type="submit" name="submit" value="Search"/>
-			</form>
-			</div>
-		</div>
-	</div>
-<div class="navsection templete">
-	<ul>
-		<li><a id="active" href="index.html">Home</a></li>
-		<li><a href="about.html">About</a></li>	
-		<li><a href="contact.html">Contact</a></li>
-	</ul>
-</div>
-
-	
-<div class="slidersection templete clear">
-        <div id="slider">
-            <a href="#"><img src="images/slideshow/01.jpg" alt="nature 1" title="This is slider one Title or Description" /></a>
-            <a href="#"><img src="images/slideshow/02.jpg" alt="nature 2" title="This is slider Two Title or Description" /></a>
-            <a href="#"><img src="images/slideshow/03.jpg" alt="nature 3" title="This is slider three Title or Description" /></a>
-            <a href="#"><img src="images/slideshow/04.jpg" alt="nature 4" title="This is slider four Title or Description" /></a>
-        </div>
-
-</div>
-	<div class="contentsection contemplete clear">
+<?php include './inc/header.php' ?>
+<?php include './inc/slider.php' ?>
+<div class="contentsection contemplete clear">
 		<div class="maincontent clear">
 			<div class="samepost clear">
 				<h2><a href="">Our post title here</a></h2>
@@ -87,7 +10,7 @@ $(window).load(function() {
 					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.
 				</p>
 				<div class="readmore clear">
-					<a href="post.html">Read More</a>
+					<a href="post.php">Read More</a>
 				</div>
 			</div>
 			<div class="samepost clear">
@@ -98,7 +21,7 @@ $(window).load(function() {
 					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.
 				</p>
 				<div class="readmore clear">
-					<a href="post.html">Read More</a>
+					<a href="post.php">Read More</a>
 				</div>
 			</div>
 			<div class="samepost clear">
@@ -109,7 +32,7 @@ $(window).load(function() {
 					Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here. Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.Some text will be go here.
 				</p>
 				<div class="readmore clear">
-					<a href="post.html">Read More</a>
+					<a href="post.php">Read More</a>
 				</div>
 			</div>
 
@@ -156,24 +79,4 @@ $(window).load(function() {
 			
 		</div>
 	</div>
-
-	<div class="footersection templete clear">
-	  <div class="footermenu clear">
-		<ul>
-			<li><a href="#">Home</a></li>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Contact</a></li>
-			<li><a href="#">Privacy</a></li>
-		</ul>
-	  </div>
-	  <p>&copy; Copyright Training with live project.</p>
-	</div>
-	<div class="fixedicon clear">
-		<a href="http://www.facebook.com"><img src="images/fb.png" alt="Facebook"/></a>
-		<a href="http://www.twitter.com"><img src="images/tw.png" alt="Twitter"/></a>
-		<a href="http://www.linkedin.com"><img src="images/in.png" alt="LinkedIn"/></a>
-		<a href="http://www.google.com"><img src="images/gl.png" alt="GooglePlus"/></a>
-	</div>
-<script type="text/javascript" src="js/scrolltop.js"></script>
-</body>
-</html>
+<?php include './inc/footer.php' ?>
